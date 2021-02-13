e54424317fc2c8e4044cab21775b9d4dc5bae3ff
Simon Kofod <skf@novozymes.com>
2021-02-13 17:43:27 +0100

    Read opencover file and extract coverage

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
 # dotnet-code-coverage-badge
 A GitHub action that generates a code coverage badge
+
+
+# Notes
+* The coverage report must be stored in ```utf8``` encoding
+
+# References
+* [GitHub Documentation](https://docs.github.com/en/actions/creating-actions/creating-a-javascript-action)
+* [Action Tutorial](https://medium.com/better-programming/4-steps-to-creating-a-custom-github-action-d67c4cf0445a)
+* [Dynamic Batch Action](https://github.com/Schneegans/dynamic-badges-action)
+
+
+# TODO
+* Generate a real coverage.opencover.xml file
--- index.js
+++ index.js
@@ -1,19 +1,39 @@
 const core = require('@actions/core');
 const http = require('https');
+const fs = require('fs')
 
 try {
-  const opencoverPath = core.getInput('opencover_path');
+  const opencoverPath = 'test/coverage.opencover.xml'//core.getInput('opencover_path');
   
-  console.log("OpencoverPath: " + opencoverPath)
-  
-  //TODO: Extract coverage from opencover file
+  var opencoverReport = readOpencoverFile(opencoverPath);
+  var coveragePercentage = extractSummaryFromOpencover(opencoverReport);
 
   //TODO: Generate json that shields.io can use
 
   //TODO: Upload the json to a gist.
 
-  core.setOutput("coverage_percentage", 0);
+  core.setOutput("coverage_percentage", coveragePercentage);
 
 } catch (error) {
   core.setFailed(error);
+}
+
+function readOpencoverFile(path) {
+  if (!fs.existsSync(opencoverPath)) {
+    throw new Error('The Opencover file was not found at the location: "' + opencoverPath + '"'); 
+  }
+
+  return fs.readFileSync(opencoverPath, 'utf8');
+}
+
+
+function extractSummaryFromOpencover(content) {
+  var rx = /(?<=sequenceCoverage=")\d*\.*\d*(?=")/m;
+  var arr = rx.exec(content);
+
+  if (arr == null) {
+    throw new Error('No code coverage percentage was found in the provided opencover report. Was looking for an xml elemet named Summary with the attribute sequenceCoverage');
+  }
+
+  return arr[0]; 
 }
